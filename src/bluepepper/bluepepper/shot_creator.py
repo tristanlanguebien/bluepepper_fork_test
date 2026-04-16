5ff536b43df98a59325f03e83beae37bbe88f388
Tristan Languebien <tristan.languebien@bigcompany.fr>
2026-04-16 14:14:20 +0200

    fix aquarium functions being triggered

--- src/bluepepper/bluepepper/shot_creator.py
+++ src/bluepepper/bluepepper/shot_creator.py
@@ -4,6 +4,7 @@ from typing import Dict, Optional
 
 from aquarium.aquarium import Shot as AqShot
 from bson import ObjectId
+from conf.project import ProjectSettings
 
 from bluepepper.aqua import get_aqua
 from bluepepper.core import codex, database, init_logging
@@ -26,11 +27,13 @@ class ShotCreator:
             self.check_required_fields()
             self.check_field_rules()
             self.create_db_document()
-            self.create_aquarium_shot()
+            if "aquarium" in ProjectSettings.production_trackers:
+                self.create_aquarium_shot()
         except Exception:
             logging.error("An error occured while creating the shot")
             self.remove_db_document()
-            self.remove_aquarium_shot()
+            if "aquarium" in ProjectSettings.production_trackers:
+                self.remove_aquarium_shot()
             raise
 
     def check_required_fields(self):
@@ -56,22 +59,14 @@ class ShotCreator:
         if not result.inserted_id:
             raise RuntimeError(f"Failed to create shot on database : {self.fields}")
         self.document = database.get_shot_document_by_id(str(result.inserted_id))
-        logging.info(
-            f"Successfully created shot document on the database : {self.document}"
-        )
+        logging.info(f"Successfully created shot document on the database : {self.document}")
         return self.document
 
     def check_existing_shot(self) -> None:
-        query = {
-            key: value
-            for key, value in self.fields.items()
-            if key in codex.convs.shot_identifier.required_fields
-        }
+        query = {key: value for key, value in self.fields.items() if key in codex.convs.shot_identifier.required_fields}
         result = list(database.shots.find(query))
         if result:
-            raise ShotAlreadyExistsError(
-                f"An shot with these fields already exists : {query}"
-            )
+            raise ShotAlreadyExistsError(f"An shot with these fields already exists : {query}")
 
     def remove_db_document(self):
         if not self.document:
@@ -81,6 +76,8 @@ class ShotCreator:
 
     def create_aquarium_shot(self):
         """Creates the shot on aquarium, and fills the "_aquariumKey" field on the database"""
+        if "aquarium" not in ProjectSettings.production_trackers:
+            return
         aqua = get_aqua()
         aq_shot = aqua.create_shot(self.document)
         self.aquarium_shot = aq_shot
@@ -90,6 +87,9 @@ class ShotCreator:
         )
 
     def remove_aquarium_shot(self) -> None:
+        if "aquarium" not in ProjectSettings.production_trackers:
+            return
+
         if not self.aquarium_shot:
             return
         logging.warning("Removing aquarium shot")
